5411f172e1227ad5992f622b01b009d5913c42aa
goyalparth61-netizen <goyalparth61@gmail.com>
2026-09-21 01:58:47 +0530

    Add reusable Ankahi Manzil brand logo

---
+++ src/components/BrandLogo.jsx
@@ -0,0 +1,10 @@
+export default function BrandLogo({ className = '' }) {
+  return (
+    <img
+      src="/images/ankahi-brand-logo.webp"
+      alt="Ankahi Manzil"
+      className={className}
+      draggable="false"
+    />
+  )
+}
