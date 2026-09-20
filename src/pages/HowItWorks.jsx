3f496606c709a11c7072b4667fec09d112cfb286
goyalparth61-netizen <goyalparth61@gmail.com>
2026-09-20 18:45:22 +0530

    Redesign how it works presentation shell

--- src/pages/HowItWorks.jsx
+++ src/pages/HowItWorks.jsx
@@ -33,14 +33,15 @@ export default function HowItWorks() {
 
   return (
     <PageTransition>
+      <div className="how-page">
       {/* Hero */}
-      <section className="relative pt-32 lg:pt-40 pb-20 overflow-hidden">
+      <section className="editorial-hero relative overflow-hidden">
         <div className="absolute inset-0 pointer-events-none">
           <div className="absolute top-1/4 left-1/3 w-[500px] h-[500px] bg-am-purple/5 rounded-full blur-[120px]" />
           <div className="absolute bottom-1/4 right-1/4 w-[400px] h-[400px] bg-am-cyan/5 rounded-full blur-[100px]" />
         </div>
 
-        <div className="container-max mx-auto px-4 lg:px-8 text-center relative">
+        <div className="page-shell relative">
           <motion.div
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
@@ -452,6 +453,7 @@ export default function HowItWorks() {
             </Link>
           </div>
         </SectionBlock>
+      </div>
       </div>
     </PageTransition>
   )
