1d28f7aacaaa13d755a402134cf8b5aa8a21335d
goyalparth61-netizen <goyalparth61@gmail.com>
2026-09-20 19:13:09 +0530

    Fix History page lint import

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion'
-import { ArrowRight, Compass, Route, Sparkles } from 'lucide-react'
+import { ArrowRight, Route, Sparkles } from 'lucide-react'
 import { Link } from 'react-router-dom'
 import PageTransition from '../components/layout/PageTransition'
 
